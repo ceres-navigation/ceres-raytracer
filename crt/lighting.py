966138b1870cf4ff326d99e311812c01778c333c
Chris Gnam <crgnam@buffalo.edu>
2022-06-10 14:49:40 -0400

    updated python wrappers for dynamic scenes and cornell_box scene

--- crt/lighting.py
+++ crt/lighting.py
@@ -13,5 +13,23 @@ class PointLight:
         self._cpp.set_position(self.position)
 
 class SquareLight:
-    def __init__():
-        return
+    def __init__(self, intensity, size, position=np.zeros(3),rotation=np.eye(3)):
+        self.intensity = intensity
+        self.size = size
+        self.position = position
+        self.rotation = rotation
+        self._cpp = _crt.SquareLight(self.intensity, self.size)
+        self._cpp.set_pose(self.position, self.rotation)
+
+    def set_position(self, position):
+        self.position = position
+        self._cpp.set_position(self.position)
+
+    def set_rotation(self, rotation):
+        self.rotation = rotation
+        self._cpp.set_rotation(self.rotation)
+
+    def set_pose(self, position, rotation):
+        self.position = position
+        self.rotation = rotation
+        self._cpp.set_pose(self.position, self.rotation)
